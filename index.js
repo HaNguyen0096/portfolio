d4d46c683feab88474a39839cab0c7733ac6ed6d
Ha Nguyen <hanguyen.0096@gmail.com>
2019-08-11 23:52:39 -0400

    make navbar responsive

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 $(".jumper").on("click", function( e ) {
     
-    //e.preventDefault();
+    e.preventDefault();
 
     $("body, html").animate({ 
         scrollTop: $( $(this).attr('href') ).offset().top 
     }, 10);
     
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+

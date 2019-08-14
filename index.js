f4c18fea60b83c267b637a431a383dd002f30410
Ha Nguyen <hanguyen.0096@gmail.com>
2019-08-14 14:03:08 -0400

    finalizing portfolio

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-$(".jumper").on("click", function( e ) {
+
+
+function scrollTop(){
+  $(".jumper").on("click", function( e ) {
     
     e.preventDefault();
 
     $("body, html").animate({ 
         scrollTop: $( $(this).attr('href') ).offset().top 
     }, slow);
-    
-});
+  });
+}
+
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -15,6 +19,9 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
-  }
+}
 
-
+function st(){
+    scrollTop();
+    $(".topnav a:not(:first-child)").css("display", "none");
+}
